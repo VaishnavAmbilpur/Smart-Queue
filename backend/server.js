814ec3c6e070c50558c089af88450cfc31415161
VaishnavAmbilpur <vaishnavambilpur2006@gmail.com>
2025-12-24 00:20:32 +0530

    Route Updated

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://smart-queue-flame.vercel.app/",
   credentials: true
 }));
 app.use(express.json());
@@ -31,7 +31,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://smart-queue-flame.vercel.app/",
     methods: ["GET", "POST","PUT"],
     credentials: true
   }
